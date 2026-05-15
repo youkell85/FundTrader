2fb41f0a725f6f49723cd7187d7d040503430461
陈川 <17063352+youkell@user.noreply.gitee.com>
2026-05-16 03:21:15 +0800

    fix: set base path /fund/ and relative tRPC URL for deployment

--- v2/frontend/vite.config.ts
+++ v2/frontend/vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  base: "/fund/",
   plugins: [
     devServer({ entry: "api/boot.ts", exclude: [/^\/(?!api\/).*$/] }),
     react(),
